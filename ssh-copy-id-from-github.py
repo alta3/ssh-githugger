b44f7d9fc7624700046ca9f2b80544fa26fa6eac
Sam Griffith <47530477+sgriffith3@users.noreply.github.com>
2020-07-06 13:29:24 -0400

    Update ssh-copy-id-from-github.py

--- ssh-copy-id-from-github.py
+++ ssh-copy-id-from-github.py
@@ -19,6 +19,7 @@ def parse_args():
         action="store_true",
         help="store public key source details in key annotation",
     )
+    parser.add_argument("-m", "--mirror", default="10.10.2.2", help="Use the preconfigured mirror")
     output_group = parser.add_mutually_exclusive_group()
     output_group.add_argument(
         "-O",
@@ -30,6 +31,7 @@ def parse_args():
     output_group.add_argument(
         "-f", "--file", type=str, default=None, help="store output in FILE"
     )
+    output_group.add_argument("-j", "--jsonfile", action="store_true", default=False, help="write results to /home/ubuntu/keyfile.gh")
     parser.add_argument(
         "-u",
         "--user",
@@ -50,9 +52,25 @@ if __name__ == "__main__":
         user=args.user,
         filename=args.file,
     )
-    loop.run_until_complete(ak.collect_keys())
-
+    if args.mirror:
+        host = args.mirror
+        port = "23456"
+        ssl = False
+    else:
+        host = "api.github.com"
+        ssl = True
+        port = ""
+    if args.jsonfile:
+        with open("/home/ubuntu/github_users.txt") as f:
+            txt = f.read().split(' ')
+            g_users = [ u.strip() for u in txt]
+        ak.github_users = g_users
+        loop.run_until_complete(ak.jsonize())
+        ak.json_to_file()
+    else:
+        loop.run_until_complete(ak.collect_keys(host=host, is_ssl=ssl, port=port))
     if args.stdout:
         print(ak.serialize())
     else:
         ak.writefile()
+                                      
